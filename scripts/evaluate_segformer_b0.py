5e59e5931c4904e8a6ef2c547156acbd0e704796
rogalikkkkkk <dmitriy.zharkov.2001@yandex.ru>
2026-05-27 00:59:00 +0300

    centralize segmentation metrics calculations

--- scripts/evaluate_segformer_b0.py
+++ scripts/evaluate_segformer_b0.py
@@ -28,58 +28,16 @@ from config import (
 from datasets.rugd_dataset import RUGDDataset
 from experiment_utils import get_run_artifact_path
 from models.segformer_b0 import create_segformer_b0
+from segmentation_metrics import (
+    calculate_metrics,
+    get_class_status,
+    update_confusion_matrix,
+)
 
 
 def synchronize_device(device):
     if device.type == "cuda":
         torch.cuda.synchronize(device)
-
-
-def update_confusion_matrix(confusion_matrix, predictions, targets, num_classes):
-    valid_pixels = (targets >= 0) & (targets < num_classes)
-
-    encoded = num_classes * targets[valid_pixels] + predictions[valid_pixels]
-    batch_confusion = np.bincount(
-        encoded,
-        minlength=num_classes * num_classes,
-    )
-    batch_confusion = batch_confusion.reshape(num_classes, num_classes)
-
-    confusion_matrix += batch_confusion
-
-
-def calculate_metrics(confusion_matrix):
-    true_positive = np.diag(confusion_matrix)
-    ground_truth_pixels = confusion_matrix.sum(axis=1)
-    predicted_pixels = confusion_matrix.sum(axis=0)
-
-    total_correct = true_positive.sum()
-    total_pixels = confusion_matrix.sum()
-    pixel_accuracy = total_correct / total_pixels
-
-    union = ground_truth_pixels + predicted_pixels - true_positive
-    valid_classes = union > 0
-    iou_per_class = np.full(confusion_matrix.shape[0], np.nan, dtype=np.float64)
-    iou_per_class[valid_classes] = true_positive[valid_classes] / union[valid_classes]
-    mean_iou = np.nanmean(iou_per_class)
-
-    return pixel_accuracy, mean_iou, iou_per_class
-
-
-def get_class_status(gt_pixels, pred_pixels, true_positive):
-    if gt_pixels == 0 and pred_pixels == 0:
-        return "absent_in_gt_and_prediction"
-
-    if gt_pixels == 0 and pred_pixels > 0:
-        return "absent_in_gt_false_positive"
-
-    if gt_pixels > 0 and pred_pixels == 0:
-        return "present_in_gt_not_predicted"
-
-    if true_positive == 0:
-        return "present_but_no_true_positive"
-
-    return "present"
 
 
 def write_metrics_file(
